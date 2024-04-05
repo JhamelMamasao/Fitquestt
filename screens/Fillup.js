1b491f241e3df5a0ede1b9a948bf31dc7a637a9a
Gerardo A. Ballester Jr. <gerardoaballesterjr@gmail.com>
2024-04-05 22:13:10 +0800

    fix bugs, integration...

--- screens/Fillup.js
+++ screens/Fillup.js
@@ -1,101 +1,138 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Pressable, TextInput, Text, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Pressable,
+  TextInput,
+  Text,
+  Image,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Picker } from '@react-native-picker/picker';
+import { Picker } from "@react-native-picker/picker";
 import { Color } from "../GlobalStyle";
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from "expo-splash-screen";
 import { SafeAreaView } from "react-native-safe-area-context";
-import * as ImagePicker from 'expo-image-picker';
-import { useFonts } from 'expo-font';
+import * as ImagePicker from "expo-image-picker";
+import { useFonts } from "expo-font";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { jwtDecode } from "jwt-decode";
 
 const Fillup = () => {
   const navigation = useNavigation();
-  const [first_name, setfirst_name] = useState('');
-  const [last_name, setlast_name] = useState('');
-  const [gender, setGender] = useState('');
-  const [height, setHeight] = useState('');
-  const [weight, setweight] = useState('');
-  const [birth_date, setbirth_date] = useState('');
-  const [nameError, setNameError] = useState('');
-  const [heightError, setHeightError] = useState('');
-  const [weightError, setweightError] = useState('');
-  const [birth_dateError, setbirth_dateError] = useState('');
+  const [first_name, setfirst_name] = useState("");
+  const [last_name, setlast_name] = useState("");
+  const [gender, setGender] = useState("");
+  const [height, setHeight] = useState("");
+  const [weight, setweight] = useState("");
+  const [birth_date, setbirth_date] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [heightError, setHeightError] = useState("");
+  const [weightError, setweightError] = useState("");
+  const [birth_dateError, setbirth_dateError] = useState("");
   const [image, setImage] = useState(null);
   const [fontError, setFontError] = useState(false);
 
   const Logout = () => {
-    navigation.navigate('Opening');
+    navigation.navigate("Opening");
   };
 
-  const FinalDashboard = () => {
+  const FinalDashboard = async () => {
     if (validateForm()) {
-      const endpoint = 'https://fitquest-8it9.onrender.com/api/account/update';
-  
-      const formData = new FormData();
-      formData.append('first_name', first_name);
-      formData.append('last_name', last_name);
-      formData.append('gender', gender);
-      formData.append('height', height);
-      formData.append('weight', weight);
-      formData.append('birth_date', birth_date);
-  
+      const endpoint = "https://fitquest-8it9.onrender.com/api/account/update";
+
+      let formData = new FormData(); // use let or var not const... const can't be modified
+      formData.append("username", await AsyncStorage.getItem("username")); // need username
+      formData.append("first_name", first_name);
+      formData.append("last_name", last_name);
+      formData.append("height", +height); // dont remove '+' it convert str to int
+      formData.append("weight", +weight); // dont remove '+' it convert str to int
+      formData.append("birth_date", birth_date);
+
+      const access = await AsyncStorage.getItem("access"); // get access token
+
       fetch(endpoint, {
-        method: 'PUT',
+        method: "PATCH",
         body: formData,
+        headers: {
+          Authorization: `Bearer ${access}`, // need this if need authorization
+          "Content-type": "multipart/form-data",
+        },
       })
-        .then((response) => {
+        .then(async (response) => {
+          console.log(response);
           if (response.ok) {
-            return response.json();
+            const data = await response.json();
+            const decoded = jwtDecode(data.access); // decode the access token
+
+            await AsyncStorage.setItem("access", data.access); // save the access token
+            await AsyncStorage.setItem("refresh", data.refresh); // save the refresh token
+
+            await AsyncStorage.setItem("user_id", decoded.user_id.toString()); // save the decoded user_id
+            await AsyncStorage.setItem("first_name", decoded.first_name); // save the decoded first_name
+            await AsyncStorage.setItem("last_name", decoded.last_name); // save the decoded last_name
+            await AsyncStorage.setItem("birth_date", decoded.birth_date); // save the decoded birth_date
+            await AsyncStorage.setItem("email", decoded.email); // save the decoded email
+            await AsyncStorage.setItem("username", decoded.username); // save the decoded username
+            await AsyncStorage.setItem(
+              "height",
+              decoded.height ? decoded.height.toString() : "0"
+            ); // save the decoded height
+            await AsyncStorage.setItem(
+              "weight",
+              decoded.weight ? decoded.weight.toString() : "0"
+            ); // save the decoded weight
+            await AsyncStorage.setItem("profile", decoded.profile); // save the decoded profile
+            await AsyncStorage.setItem("slug", decoded.slug); // save the decoded slug
+
+            console.log("1111111111111111111111");
+
+            navigation.navigate("Dashboard");
           } else {
-            // Handle non-200 status codes
-            throw new Error(Request failed with status ${response.status}: ${response.statusText});
+            throw new Error(response);
           }
         })
         .then((result) => {
-          // Handle the response data
-          console.log('API response:', result);
-          navigation.navigate('Dashboard');
+          console.log("API response:", result);
+          navigation.navigate("Dashboard");
         })
         .catch((error) => {
-          // Handle network errors or other issues
-          console.error('Error during API request:', error.message);
-          // You can also display a user-friendly error message to the user
-          // e.g., toast or modal with "Something went wrong. Please try again later."
+          console.log(error);
+          // console.error("Error during API request:", error.message);
         });
     }
-  };  
+  };
 
   const validateForm = () => {
     let valid = true;
-    if (first_name.trim() === '') {
-      setNameError('Name is required');
+    if (first_name.trim() === "") {
+      setNameError("Name is required");
       valid = false;
     } else {
-      setNameError('');
+      setNameError("");
     }
-    if (last_name.trim() === '') {
-      setNameError('Name is required');
+    if (last_name.trim() === "") {
+      setNameError("Name is required");
       valid = false;
     } else {
-      setNameError('');
+      setNameError("");
     }
-    if (isNaN(height) || height.trim() === '') {
-      setHeightError('Height must be a number');
+    if (isNaN(height) || height.trim() === "") {
+      setHeightError("Height must be a number");
       valid = false;
     } else {
-      setHeightError('');
+      setHeightError("");
     }
-    if (isNaN(weight) || weight.trim() === '') {
-      setweightError('Weight must be a number');
+    if (isNaN(weight) || weight.trim() === "") {
+      setweightError("Weight must be a number");
       valid = false;
     } else {
-      setweightError('');
+      setweightError("");
     }
-    if (birth_date.trim() === '') {
-      setbirth_dateError('Birth date is required');
+    if (birth_date.trim() === "") {
+      setbirth_dateError("Birth date is required");
       valid = false;
     } else {
-      setbirth_dateError('');
+      setbirth_dateError("");
     }
     return valid;
   };
@@ -111,9 +148,9 @@ const Fillup = () => {
   };
 
   let [fontsLoaded] = useFonts({
-    "Poppins-Medium": require('../assets/fonts/Poppins-Medium.ttf'),
-    "Poppins-Bold": require('../assets/fonts/Poppins-Bold.ttf'),
-    "Poppins-Black": require('../assets/fonts/Poppins-Black.ttf'),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
   });
 
   const onLayoutRootView = React.useCallback(async () => {
@@ -131,9 +168,14 @@ const Fillup = () => {
       <View style={styles.images}>
         <View style={styles.Circle}>
           <Pressable style={styles.socialIcon} onPress={pickImage}>
-            <Image source={require('../assets/images/upload.png')} style={styles.icon} />
+            <Image
+              source={require("../assets/images/upload.png")}
+              style={styles.icon}
+            />
           </Pressable>
-          {image && <Image source={{ uri: image }} style={styles.circleImage} />}
+          {image && (
+            <Image source={{ uri: image }} style={styles.circleImage} />
+          )}
         </View>
       </View>
 
@@ -144,8 +186,7 @@ const Fillup = () => {
           onChangeText={(text) => setfirst_name(text)}
           value={first_name}
         />
-        {nameError !== '' && 
-        <Text style={styles.error}>{nameError}</Text>}
+        {nameError !== "" && <Text style={styles.error}>{nameError}</Text>}
       </View>
 
       <View style={styles.inputContainer}>
@@ -155,8 +196,7 @@ const Fillup = () => {
           onChangeText={(text) => setlast_name(text)}
           value={last_name}
         />
-        {nameError !== '' && 
-        <Text style={styles.error}>{nameError}</Text>}
+        {nameError !== "" && <Text style={styles.error}>{nameError}</Text>}
       </View>
 
       <View style={styles.inputContainer}>
@@ -167,8 +207,9 @@ const Fillup = () => {
           value={birth_date}
           placeholder="YYYY-MM-DD"
         />
-        {birth_dateError !== '' && 
-        <Text style={styles.error}>{birth_dateError}</Text>}
+        {birth_dateError !== "" && (
+          <Text style={styles.error}>{birth_dateError}</Text>
+        )}
       </View>
 
       <View style={styles.inputContainer}>
@@ -176,16 +217,18 @@ const Fillup = () => {
         <Picker
           selectedValue={height}
           style={styles.input}
-          onValueChange={(itemValue, itemIndex) =>
-            setHeight(itemValue)
-          }>
+          onValueChange={(itemValue, itemIndex) => setHeight(itemValue)}
+        >
           <Picker.Item label="Select Height" value="" />
-          {Array.from({length: 150}, (_, i) => i + 100).map(value => (
-            <Picker.Item key={value.toString()} label={`${value} cm`} value={value.toString()} />
+          {Array.from({ length: 150 }, (_, i) => i + 100).map((value) => (
+            <Picker.Item
+              key={value.toString()}
+              label={`${value} cm`}
+              value={value.toString()}
+            />
           ))}
         </Picker>
-        {heightError !== '' && 
-        <Text style={styles.error}>{heightError}</Text>}
+        {heightError !== "" && <Text style={styles.error}>{heightError}</Text>}
       </View>
 
       <View style={styles.inputContainer}>
@@ -193,16 +236,18 @@ const Fillup = () => {
         <Picker
           selectedValue={weight}
           style={styles.input}
-          onValueChange={(itemValue, itemIndex) =>
-            setweight(itemValue)
-          }>
-          <Picker.Item label="Select Weight" value=""/>
-          {Array.from({length: 150}, (_, i) => i + 50).map(value => (
-            <Picker.Item key={value.toString()} label={`${value} kg`} value={value.toString()} />
+          onValueChange={(itemValue, itemIndex) => setweight(itemValue)}
+        >
+          <Picker.Item label="Select Weight" value="" />
+          {Array.from({ length: 150 }, (_, i) => i + 50).map((value) => (
+            <Picker.Item
+              key={value.toString()}
+              label={`${value} kg`}
+              value={value.toString()}
+            />
           ))}
         </Picker>
-        {weightError !== '' && 
-        <Text style={styles.error}>{weightError}</Text>}
+        {weightError !== "" && <Text style={styles.error}>{weightError}</Text>}
       </View>
 
       <View style={styles.ButtonContainers}>
@@ -217,9 +262,9 @@ const Fillup = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
   images: {
     width: 90,
@@ -232,40 +277,40 @@ const styles = StyleSheet.create({
     width: 25,
     height: 25,
     borderRadius: 100 / 2,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
     marginTop: 70,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 2,
   },
   icon: {
     width: 20,
     height: 20,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
   },
   inputContainer: {
     width: "80%",
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
   inputText: {
     fontSize: 13,
-    fontFamily: 'Poppins-Medium',
+    fontFamily: "Poppins-Medium",
     marginTop: 10,
-    textAlign: 'left', 
+    textAlign: "left",
   },
   input: {
-    width: "100%", 
+    width: "100%",
     height: 45,
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
     paddingHorizontal: 10,
     borderRadius: 5,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.3,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 5,
   },
   error: {
-    color: 'red',
+    color: "red",
     fontSize: 12,
   },
   submitButtonText: {
@@ -286,7 +331,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   ButtonContainers: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 5,
   },
 });
--- screens/Register.js
+++ screens/Register.js
@@ -1,54 +1,95 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Pressable, Text, TextInput, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Pressable,
+  Text,
+  TextInput,
+  Image,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Color } from "../GlobalStyle";
 import { SafeAreaView } from "react-native-safe-area-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { jwtDecode } from "jwt-decode";
 
 const Register = () => {
   const navigation = useNavigation();
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password1, setPassword1] = useState('');
-  const [password2, setPassword2] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [error, setError] = useState("");
 
   const handleSignUp = async () => {
     try {
       if (!email || !username || !password1 || !password2) {
-        setError('Please fill in all fields');
+        setError("Please fill in all fields");
         return;
       }
 
-      console.log("Signing up with:", { email, username, password1, password2 });
-
-      const response = await fetch('https://fitquest-8it9.onrender.com/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          username,
-          password1,
-          password2,
-        }),
+      console.log("Signing up with:", {
+        email,
+        username,
+        password1,
+        password2,
       });
 
+      const response = await fetch(
+        "https://fitquest-8it9.onrender.com/api/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            username,
+            password1,
+            password2,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
       if (response.ok) {
-        navigation.navigate('Fillup'); // Navigate to success screen or next step
+        navigation.navigate("Fillup");
+        const decoded = jwtDecode(data.access); // decode the access token
+
+        console.log(decoded);
+
+        await AsyncStorage.setItem("access", data.access); // save the access token
+        await AsyncStorage.setItem("refresh", data.refresh); // save the refresh token
+
+        await AsyncStorage.setItem("user_id", decoded.user_id.toString()); // save the decoded user_id
+        await AsyncStorage.setItem("first_name", decoded.first_name); // save the decoded first_name
+        await AsyncStorage.setItem("last_name", decoded.last_name); // save the decoded last_name
+        await AsyncStorage.setItem("birth_date", decoded.birth_date); // save the decoded birth_date
+        await AsyncStorage.setItem("email", decoded.email); // save the decoded email
+        await AsyncStorage.setItem("username", decoded.username); // save the decoded username
+        await AsyncStorage.setItem(
+          "height",
+          decoded.height ? decoded.height.toString() : "0"
+        ); // save the decoded height
+        await AsyncStorage.setItem(
+          "weight",
+          decoded.weight ? decoded.weight.toString() : "0"
+        ); // save the decoded weight
+        await AsyncStorage.setItem("profile", decoded.profile); // save the decoded profile
+        await AsyncStorage.setItem("slug", decoded.slug); // save the decoded slug
       } else {
-        const data = await response.json();
         setError(data.message); // Display error message from the server
       }
     } catch (error) {
-      console.error('Error:', error);
-      setError('An error occurred. Please try again.'); // Set generic error message
+      console.error("Error:", error);
+      setError("An error occurred. Please try again."); // Set generic error message
     }
-  }
+  };
 
   const handleSignInPress = () => {
     console.log("Navigating to sign-in screen");
-    navigation.navigate('Login');
+    navigation.navigate("Login");
   };
 
   return (
@@ -96,7 +137,7 @@ const Register = () => {
         />
       </View>
 
-      {error !== '' && <Text style={styles.error}>{error}</Text>}
+      {error !== "" && <Text style={styles.error}>{error}</Text>}
 
       <Pressable style={styles.submitButton} onPress={handleSignUp}>
         <Text style={styles.submitButtonText}>Submit</Text>
@@ -104,20 +145,32 @@ const Register = () => {
 
       <Text style={styles.SocialText}>Login with</Text>
       <View style={styles.socialIconsContainer}>
-        <Pressable style={styles.socialIcon} onPress={() => console.log("Google Login")}>
-          <Image source={require('../assets/images/google.png')} style={styles.socialIconImage} />
+        <Pressable
+          style={styles.socialIcon}
+          onPress={() => console.log("Google Login")}
+        >
+          <Image
+            source={require("../assets/images/google.png")}
+            style={styles.socialIconImage}
+          />
         </Pressable>
-        <Pressable style={styles.socialIcon} onPress={() => console.log("Facebook Login")}>
-          <Image source={require('../assets/images/fb.png')} style={styles.socialIconImage} />
+        <Pressable
+          style={styles.socialIcon}
+          onPress={() => console.log("Facebook Login")}
+        >
+          <Image
+            source={require("../assets/images/fb.png")}
+            style={styles.socialIconImage}
+          />
         </Pressable>
       </View>
 
       <Pressable style={styles.Signup} onPress={handleSignInPress}>
-  <Text style={styles.textall}>
-    {"if you already have an account \n please "}
-    <Text style={styles.textSignup}>Sign In</Text>
-  </Text>
-</Pressable>
+        <Text style={styles.textall}>
+          {"if you already have an account \n please "}
+          <Text style={styles.textSignup}>Sign In</Text>
+        </Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
@@ -125,9 +178,9 @@ const Register = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
   },
   loginText: {
     fontSize: 50,
@@ -147,7 +200,7 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 10,
     borderRadius: 5,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.6,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 5,
@@ -171,7 +224,7 @@ const styles = StyleSheet.create({
     width: "80%",
     alignSelf: "center",
     marginTop: 15,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.8,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 5,
@@ -200,7 +253,7 @@ const styles = StyleSheet.create({
     height: 35,
   },
   Signup: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     marginTop: 5,
     fontFamily: "Poppins-Medium",
     right: 10,
@@ -209,20 +262,20 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     color: Color.colorDarkorange,
     fontSize: 13,
-    textAlign: 'center',
+    textAlign: "center",
     marginHorizontal: 5,
     fontFamily: "Poppins-Medium",
   },
   textall: {
     flexDirection: "row",
     fontSize: 13,
-    textAlign: 'center',
+    textAlign: "center",
     fontFamily: "Poppins-Medium",
   },
   error: {
-    color: 'red',
+    color: "red",
     fontSize: 12,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
 
